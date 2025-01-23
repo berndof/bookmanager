7eaa673f9107b503a1a6c6c2fd6cad7623118eaf
berndof <bernasoestudas@gmail.com>
2025-01-23 11:20:12 -0300

    selecting pdf and reading metadata

--- controllers/config_library_controller.py
+++ controllers/config_library_controller.py
@@ -1,8 +1,40 @@
-from views.config_library_page import ConfigLibraryPage
+from views.config_library_page import ConfigLibraryPage, Subpage0_SelectBookFile, Subpage1_EditBookInfo
+import fitz
 
 class ConfigLibraryController:
     def __init__(self, main_controller):
         self.main_controller = main_controller
-        #self.model = 
         self.view = ConfigLibraryPage(self)
+        self.create_pages()
 
+    def create_pages(self):       
+        self.subpage0_select_book_file = Subpage0_SelectBookFile(self)
+        self.add_page(self.subpage0_select_book_file) #Page 0
+
+        self.subpage1_edit_book_info = Subpage1_EditBookInfo(self)
+        self.add_page(self.subpage1_edit_book_info) #Page 1
+
+    def add_page(self, page):
+        self.view.page_stack.addWidget(page)
+
+    def go_to_page(self, index):
+        self.view.page_stack.setCurrentIndex(index)
+    
+    def go_to_select_book_file_page(self):
+        self.go_to_page(0)
+
+    def go_to_edit_book_info_page(self):
+        self.go_to_page(1)
+
+    def handle_select_file(self, file_path):
+        
+        import os
+        destination_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "books") #/books
+        
+        if not os.path.exists(destination_path):
+            os.makedirs(destination_path)
+        
+        document = fitz.open(file_path)
+
+        self.subpage1_edit_book_info.load_book_info(document)
+        self.go_to_edit_book_info_page()
--- views/config_library_page.py
+++ views/config_library_page.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel, QPushButton, QListView
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QLabel, QPushButton, QListView, QStackedWidget, QFileDialog, QMessageBox
 
 class ConfigLibraryPage(QWidget):
     def __init__(self, controller):
@@ -10,7 +10,52 @@ class ConfigLibraryPage(QWidget):
         self.layout = QVBoxLayout()
         self.setLayout(self.layout)
 
-        self.label = QLabel("Config_library_page")
+        self.label = QLabel("Configurações da Biblioteca")
         self.layout.addWidget(self.label)
 
-        
+        self.page_stack = QStackedWidget()
+        self.layout.addWidget(self.page_stack)
+
+class Subpage0_SelectBookFile(QWidget):
+    def __init__(self, controller):
+        super().__init__()
+        self.controller = controller
+        self.init_ui()
+
+    def init_ui(self):
+        self.layout = QVBoxLayout()
+        self.setLayout(self.layout)
+
+        self.label = QLabel("Selecione o arquivo de configuração")
+        self.layout.addWidget(self.label)
+
+        self.button = QPushButton("Selecionar")
+        self.button.clicked.connect(self.show_select_file_dialog)
+
+        self.layout.addWidget(self.button)
+
+    def show_select_file_dialog(self):
+        options = QFileDialog.Options()
+        file_path, _ = QFileDialog.getOpenFileName(None, "Add New Book", "", "PDF Files (*.pdf)", options=options)
+
+        if file_path.endswith('.pdf') or file_path.endswith('.epub'):
+            self.controller.handle_select_file(file_path)
+        else:
+            QMessageBox.warning(None, "Formato inválido", "Por favor, escolha um arquivo no formato PDF ou EPUB.")
+
+class Subpage1_EditBookInfo(QWidget):
+    def __init__(self, controller):
+        super().__init__()
+        self.controller = controller
+        self.init_ui()
+
+    def init_ui(self):
+        self.layout = QVBoxLayout()
+        self.setLayout(self.layout)
+
+        self.label = QLabel("Editando livro")
+        self.layout.addWidget(self.label)
+
+    def load_book_info(self, document):
+        self.document = document
+        self.label.setText(f"Editando livro: {document.metadata}")
